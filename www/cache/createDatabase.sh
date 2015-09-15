2b305d09cf6a6238154c2f91c3ef360a7226f11f
Sébastien M-B <seb@essembeh.org>
2015-09-16 00:24:32 +0200

    Better setup script for database

--- www/cache/createDatabase.sh
+++ www/cache/createDatabase.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -ex
 
-DB="azimut.sqlite"
-SCHEMA="schema.sql"
-test -f "$SCHEMA"
+ROOT=`dirname "$0"`
+DB="$ROOT/azimut.sqlite"
+SCHEMA="$ROOT/schema.sql"
 
+test -f "$SCHEMA"
 if test -f "$DB"; then
 	rm "$DB"
 fi
 
 sqlite3 "$DB" < "$SCHEMA"
-
 chmod 666 "$DB"
